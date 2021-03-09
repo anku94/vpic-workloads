30af46f75143876335db26113448e84c0fb5b937
Ankush Jain <ankushj@andrew.cmu.edu>
2021-03-09 12:44:38 -0500

    Add max/avg olap

--- src/plot_batch.py
+++ src/plot_batch.py
@@ -8,14 +8,35 @@ from pathlib import Path
 import re
 
 OVERRIDES = {
-    '/Users/schwifty/Repos/workloads/rundata/Mar8/vpic-carp8m-0-carp.38446/I-0/carp_P3584M_intvl250000':
+    '/Users/schwifty/Repos/workloads/rundata/Mar8/vpic-carp8m-0-carp.38446/I'
+    '-0/carp_P3584M_intvl250000':
         {
             'rintvl': 'once_global',
             'rundata': {
-                'mdb': [0.26, 0.27, 1.63, 3.98, 5.53]
+                'mdb': {
+                    'max':
+                        [0.26, 0.27, 1.63, 3.98, 5.53]
+                }
             }
         }
 }
+
+
+def override_exists_impl(obj, props):
+    if len(props) == 0:
+        return obj
+    elif props[0] in obj:
+        return override_exists_impl(obj[props[0]], props[1:])
+    else:
+        return None
+
+
+def override_exists(*args):
+    props = list(args)
+    if len(props) == 0:
+        return None
+    else:
+        return override_exists_impl(OVERRIDES, props)
 
 
 def get_file_params(fpath: str):
@@ -30,13 +51,13 @@ def get_file_params(fpath: str):
     if (params):
         pvtcnt = params[0]
 
-    if fpath in OVERRIDES:
-        fprops = OVERRIDES[fpath]
-        if 'rintvl' in fprops:
-            rintvl = fprops['rintvl']
+    override = override_exists(fpath, 'rintvl')
+    if override:
+        rintvl = override
 
-        if 'pvtcnt' in fprops:
-            pvtcnt = fprops['pvtcnt']
+    override = override_exists(fpath, 'pvtcnt')
+    if override:
+        pvtcnt = override
 
     return (rintvl, pvtcnt)
 
@@ -44,7 +65,6 @@ def get_file_params(fpath: str):
 def plot_runtime(jobdir: str, param: str, ax: object) -> None:
     runtime_csv = jobdir + '/plots/runtime.csv'
     runtime = pd.read_csv(runtime_csv, header=None)
-    print(runtime)
     ax.plot(range(len(runtime[0])), runtime[0], label=param)
 
 
@@ -64,16 +84,14 @@ def run_runtime(all_jobdirs: str, plot_dir: str):
     fig.savefig(plot_dir + '/runtime.pdf', dpi=300)
 
 
-def plot_olap(jobdir: str, param: str, ax: object, key: str) -> None:
-    if jobdir in OVERRIDES:
-        job_or = OVERRIDES[jobdir]
-        if 'rundata' in job_or:
-            job_rd = job_or['rundata']
-            if key in job_rd:
-                data = job_rd[key]
-                data_x = list(range(len(data)))
-                data_y = data
-                ax.plot(data_x, data_y, label=param)
+def plot_olap(jobdir: str, param: str, ax: object, key: str, op: str) -> None:
+    override = override_exists(jobdir, 'rundata', key, op)
+    if override:
+        data = override
+        data_x = list(range(len(data)))
+        data_y = data
+        ax.plot(data_x, data_y, label=param)
+        return
 
     glob_template = '/plots/{0}.olap.*csv'.format(key)
     olap_globstr = jobdir + glob_template
@@ -82,7 +100,10 @@ def plot_olap(jobdir: str, param: str, ax: object, key: str) -> None:
     for epoch_csv in epoch_olaps:
         epoch_pd = pd.read_csv(epoch_csv)
         epoch_idx = epoch_csv.split('.')[-2].strip('e')
-        mass_max = epoch_pd['MatchMass'].max()
+        if op == 'avg':
+            mass_max = epoch_pd['MatchMass'].mean()
+        else:
+            mass_max = epoch_pd['MatchMass'].max()
         mass_total = epoch_pd['TotalMass'][0]
 
         if mass_total == 0:
@@ -100,27 +121,29 @@ def plot_olap(jobdir: str, param: str, ax: object, key: str) -> None:
     ax.plot(data_x, data_y, label=param)
 
 
-def run_olap_impl(all_jobdirs: str, plot_dir: str, key: str):
+def run_olap_impl(all_jobdirs: str, plot_dir: str, key: str, op: str):
     PKEY = 0
     fig, ax = plt.subplots(1, 1)
     for dir in all_jobdirs:
         params = get_file_params(dir)
         param = params[PKEY]
-        plot_olap(dir, param, ax, key)
+        plot_olap(dir, param, ax, key, op)
 
     ax.legend()
     ax.set_ylim([0, ax.get_ylim()[1]])
     ax.set_xlabel('Epoch Index')
     ax.set_ylabel('Epoch Overlap (Percent)')
-    ax.set_title('Max {0} Overlap vs RTP Interval'.format(key.upper()))
-    plot_path = '{0}/{1}.olap.pdf'.format(plot_dir, key)
+    ax.set_title(
+        'Max {0} Overlap vs RTP Interval (Op: {1})'.format(key.upper(), op))
+    plot_path = '{0}/{1}.{2}.olap.pdf'.format(plot_dir, key, op)
     fig.savefig(plot_path, dpi=300)
 
 
 def run_olap(all_dirs: str, dir_out: str):
-    run_olap_impl(all_dirs, dir_out, 'rtp')
-    run_olap_impl(all_dirs, dir_out, 'mdb')
-    run_olap_impl(all_dirs, dir_out, 'rdb')
+    run_olap_impl(all_dirs, dir_out, 'rtp', op='max')
+    run_olap_impl(all_dirs, dir_out, 'mdb', op='max')
+    run_olap_impl(all_dirs, dir_out, 'mdb', op='avg')
+    run_olap_impl(all_dirs, dir_out, 'rdb', op='max')
 
 
 def run(dir_in: str, dir_out: str):
